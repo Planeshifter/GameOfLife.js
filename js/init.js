50ae804fb0ca6af9a73d87d5c405c710b9923de1
Planeshifter <pburckhardt@outlook.com>
2015-01-08 20:42:08 +0100

    first commit

---
+++ js/init.js
@@ -0,0 +1,156 @@
+function Cell(x, y, grid){
+  this.x = x;
+  this.y = y;
+  this.grid = grid;
+  this.alive = false;
+  this.liveNeighbours = null;
+}
+
+Cell.prototype.update = function(){
+  if (this.alive === true){
+    this.alive = (this.liveNeighbours === 2 || this.liveNeighbours === 3) ? true : false;
+  } else if (this.liveNeighbours === 3){
+    this.alive = true;
+  }
+};
+
+Cell.prototype.countLivingNeighbours = function(){
+  var count = 0;
+  var i_low =  Math.max(this.y - 1, 0);
+  var i_high = Math.min(this.y + 1, this.grid.height - 1);
+  var j_low = Math.max(this.x - 1, 0);
+  var j_high = Math.min(this.x + 1, this.grid.width - 1);
+  for (var i = i_low; i <= i_high; i++){
+    for (var j = j_low; j <= j_high; j++){
+      if (this.grid.rows[i][j].alive === true && !(i === this.y && j === this.x)) count++;
+    }
+  }
+  this.liveNeighbours = count;
+};
+
+function View(grid){
+  var self = this;
+  this.table = $("#grid_table");
+
+  this.update = function () {
+      grid.traverse(function (cell) {
+          if (cell.alive === true) {
+              cell.element.addClass('alive');
+          } else {
+              cell.element.removeClass('alive');
+          }
+      });
+  };
+
+  this.init = function(){
+     self.table.empty();
+     self.table.addClass("grid_table");
+          var x, y, tr, td, cell;
+          allCells = [];
+          var toggleCell = function (cell) {
+              return function (e) {
+                  cell.alive = !cell.alive;
+              };
+          };
+
+          for (y = 0; y < grid.height; y++) {
+              tr = $('<tr>');
+              for (x = 0; x < grid.width; x++) {
+                  td = $("<td>");
+                  td.addClass('cell');
+
+                  cell = grid.rows[y][x];
+                  cell.element = td;
+
+                  td.attr("x",x);
+                  td.attr("y",y);
+
+                  tr.append(td);
+              }
+              self.table.append(tr);
+          }
+
+          $("#grid_table td").click(function(){
+              cell = grid.rows[$(this).attr("y")][$(this).attr("x")];
+              cell.alive = cell.alive ? false : true;
+              self.update();
+          });
+  };
+}
+
+function Grid(gridWidth, gridHeight){
+    var self = this;
+    this.width = gridWidth || 50;
+    this.height = gridHeight || 50;
+    this.rows = [];
+    this.nsteps = null;
+    this.current_step = null;
+    this.timer = null;
+    this.view = null;
+
+    this.init = function(){
+            var row;
+            self.rows = [];
+            for (var y = 0; y < self.height; y++) {
+                row = [];
+                for (var x = 0; x < self.width; x++) {
+                    var ncell = new Cell(x, y, self);
+                    row.push(ncell);
+                }
+                self.rows.push(row);
+            }
+    };
+
+    this.resize = function(gridWidth, gridHeight){
+      self.width = gridWidth || 50;
+      self.height = gridHeight || 50;
+      self.init();
+    };
+
+    this.traverse = function(callback){
+      for (var y = 0; y < self.height; y++) {
+        for (var x = 0; x < self.width; x++) {
+          callback(self.rows[y][x], x, y);
+        }
+      }
+    };
+
+    this.step = function(){
+      if(self.current_step < self.nsteps){
+        console.log("Calculating round " + self.current_step);
+        self.traverse(function(cell){
+          cell.countLivingNeighbours();
+        });
+        self.traverse(function(cell){
+          cell.update();
+        });
+        self.view.update();
+        self.current_step++;
+      } else {
+        clearInterval(self.timer);
+      }
+    };
+}
+
+$(document).ready(function(){
+  var grid = new Grid(25, 25);
+  var view = new View(grid);
+  grid.view = view;
+  grid.init();
+  view.init();
+
+  $('#create_grid_btn').click(function(){
+    var width, height;
+    width = $("#input_width").val();
+    height = $("#input_height").val();
+    grid.resize(width, height);
+    view.init();
+  });
+
+  $('#start_game_btn').click(function(){
+    var speed = $("#speed_input").val() || 200;
+    grid.nsteps = $("#nsteps_input").val() || 10;
+    grid.current_step = 0;
+    grid.timer = setInterval(grid.step, speed);
+  });
+});
